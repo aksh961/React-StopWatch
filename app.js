58fc5adb25605391c7e65ac6ffac8c5587e8e73c
AKSHnav <72068211+aksh961@users.noreply.github.com>
2023-06-19 12:38:43 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Stopwatch from './components/Stopwatch/Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
+stopwatch.jsx
